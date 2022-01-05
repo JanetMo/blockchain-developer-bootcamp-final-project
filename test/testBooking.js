7a2acf9c89d4381b98a9fe8f7901ebb0cdd9b207
JanetMo <m.marz@kabelmail.de>
2022-01-05 19:47:08 +0100

    test

--- test/testBooking.js
+++ test/testBooking.js
@@ -6,8 +6,11 @@ contract("Booking", (accounts) => {
   //importing the Booking-contact and providing the available accounts
 
   before(async () => {
-      booking = await Booking.deployed();
+    booking = await Booking.deployed();
   });
+
+  //Testing the book-function: book the appointment with id 3 and assign it to the account 0
+  //should return the expected patient
 
   describe("adopting a pet and retrieving account addresses", async () => {
     before("adopt a pet using accounts[0]", async () => {
@@ -15,27 +18,31 @@ contract("Booking", (accounts) => {
       expectedPatient = accounts[0];
     });
 
-//Testing the book-function: book the appointment with id 3 and assign it to the account 0 
-//should return the expected patient
-    
-      it("can fetch the address of an owner by pet id", async () => {
-        const patient = await booking.patients(3);
-        assert.equal(patient, expectedPatient, "The owner of the adopted pet should be the first account.");
-      });
-    
-  //Call the patients smart contract method to see the address of the patient who booked the appointment with Id 3.
-  // Then we test the retrieval of all patients who booked an appointment.
+    it("can fetch the address of an owner by pet id", async () => {
+      const patient = await booking.patients(3);
+      assert.equal(
+        patient,
+        expectedPatient,
+        "The owner of the adopted pet should be the first account."
+      );
+    });
 
-      it("can fetch the collection of all pet owners' addresses", async () => {
-        const patients = await booking.getPatients();
-        assert.equal(patients[3], expectedPatient, "The owner of the adopted pet should be in the collection.");
-      });
-    }); 
-  })
-  //Then we compare the contract adress with the one whe should find. 
+    //Call the patients smart contract method to see the address of the patient who booked the appointment with Id 3.
+    // Then we test the retrieval of all patients who booked an appointment.
 
+    it("can fetch the collection of all pet owners' addresses", async () => {
+      const patients = await booking.getPatients();
+      assert.equal(
+        patients[3],
+        expectedPatient,
+        "The owner of the adopted pet should be in the collection."
+      );
+    });
+    //
+    //
+    //it ("behaviour we ware looking for", async () => {
 
-
-
-
- 
+    //)
+  });
+});
+//Then we compare the contract adress with the one whe should find.
