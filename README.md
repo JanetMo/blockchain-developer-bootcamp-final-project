56af1f701f8a420cc1619b174e2e715ae9b7b71f
JanetMo <m.marz@kabelmail.de>
2022-01-05 21:05:55 +0100

    save

--- README.md
+++ README.md
@@ -33,7 +33,6 @@ To address some weaknesses, the following ideas could be implemented later:
 - Link to the App: ... (unfortunately, the hosting didn't work)
 - My recorded video using the DApp: https://www.loom.com/share/0d0b87432325410985a3514b10b3476c
 - My Ethereum Adress: 0xB34c4D25f25cC296ae4c094BaB9d49e9cce0746A
--
 
 ## How to try the app on your local machine using truffle and Ganache:
 
--- contracts/Booking.sol
+++ contracts/Booking.sol
@@ -2,28 +2,29 @@
 pragma solidity 0.8.0;
 /// @title Register your medical appointment on the blockchain.
 /// @author JanetMo
-/// @notice This is a test contract, it's only deployed on the testnet. And this project is my first experience with coding, so it's very basic
+/// @notice This is a test contract, it's only deployed on the testnet. 
+///custom:This project is my first experience with coding, so it's very basic
 /// custom:experimental This is an experimental contract.
 
-//import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Booking //is Ownable
-{ 
 
-address[16] public patients;
+contract Booking is Ownable { 
+
+    address[16] public patients;
 
 /// Booking a medical appointment
-function book(uint appId) public returns (uint) {
-  require(appId >= 0 && appId <= 5);
+    function book(uint appId) public returns (uint) {
+        require(appId >= 0 && appId <= 5);
 
-  patients[appId] = msg.sender;
+        patients[appId] = msg.sender;
 
-  return appId;
-  }
+        return appId;
+    }
 
   /// Retrieving the appointment
-function getPatients() public view returns (address[16] memory) {
-  return patients;
+    function getPatients() public view returns (address[16] memory) {
+        return patients;
     }
 
 
